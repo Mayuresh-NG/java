12c81c3096ee7c638d68f7248e655da6f99791f6
Mayuresh <mayureshngorantiwar@gmail.com>
2023-06-09 09:36:25 +0530

    done

---
+++ src/basic.java
@@ -0,0 +1,11 @@
+import java.io.DataInputStream;
+import java.io.*;
+import java.util.*;
+
+public class basic {
+    public static void main(String[] args){
+        int a=10;
+        System.out.println(a);
+    }
+}
+
